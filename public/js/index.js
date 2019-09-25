d9ff0e12b486bae6e5a7b63bac58b667415b565f
Oleh <cherybaoleh@gmail.com>
2019-09-25 11:48:37 +0300

    nodemon + good babel compilation having been added

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,9 @@
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const helloContinents = continents.map(el => `Hello ${el}!`);
-const message = helloContinents.join(' ');
-const element = React.createElement("div", null, React.createElement("h1", null, message));
+"use strict";
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var helloContinents = continents.map(function (el) {
+  return "Hello ".concat(el, "!");
+});
+var message = helloContinents.join(' ');
+var element = React.createElement("div", null, React.createElement("h1", null, message));
 ReactDOM.render(element, document.querySelector('#root'));
